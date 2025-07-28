870a289da04f0eb5a68d51524de3bd4b9eae030b
dhananjay-dhawale <dhananjaydhawle2000@gmail.com>
2025-07-29 00:22:39 +0530

    github changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://dhananjay-dhawale.github.io/astro-portfolio',
+  base: '/astro-portfolio',
   integrations: [
     react(),     // 👈 this enables rendering `.tsx` components
     tailwind(),
