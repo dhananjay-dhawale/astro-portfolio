8249b6b515674decd8dc2c48ff3cad5a1d6e6ec0
dhananjay-dhawale <dhananjaydhawle2000@gmail.com>
2025-07-29 01:40:35 +0530

    updated links

---
+++ src/components/Links.tsx
@@ -0,0 +1,40 @@
+// src/components/Links.tsx
+const links = [
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/dhananjay-dhawale-829659198/' },
+  { name: 'Twitter', url: 'https://twitter.com/nonchalantnerdd' },
+  { name: 'Leetcode', url: 'https://leetcode.com/piudpie/' },
+  { name: 'Medium', url: 'https://medium.com/@dhananjaydhawale9' },
+  { name: 'Codeforces', url: 'https://codeforces.com/profile/djay24' },
+  { name: 'Codechef', url: 'https://www.codechef.com/users/piudpie' },
+  { name: 'Geeks For Geeks', url: 'https://auth.geeksforgeeks.org/user/chessnoobdj/profile/' },
+  { name: 'Keybr - Touch Typing', url: 'https://www.keybr.com/profile/4491fz0' },
+  { name: 'HackerRank', url: 'https://www.hackerrank.com/chesskingdj?hr_r=1' },
+  { name: 'GitHub', url: 'https://github.com/dhananjay-dhawale' },
+  { name: 'Instagram', url: 'https://instagram.com/djayclicks' },
+  { name: 'Your Quote', url: 'https://www.yourquote.in/dhananjay-dhawle-bm8wu/quotes' },
+  { name: 'Lichess', url: 'https://lichess.org/@/D_JAY' },
+  { name: 'Quora', url: 'https://www.quora.com/profile/Dhananjay-Dhawle' },
+  { name: 'CSES', url: 'https://cses.fi/user/111362' },
+  { name: 'Duolingo', url: 'https://www.duolingo.com/profile/ph.zQ3Tfn?via=share_profile' },
+];
+
+export default function Links() {
+  return (
+    <section className="py-10 px-6 bg-black text-white max-w-3xl mx-auto text-center">
+      <h2 className="text-3xl font-bold mb-6 text-sky-400">find me online</h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {links.map(link => (
+          <a
+            key={link.name}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block px-4 py-2 rounded-full bg-white/10 hover:bg-sky-400 hover:text-black transition text-sm font-medium border border-white/20"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
