70b3f5bb42d4e12ef4df585a07e6b4385dd072a4
dhananjay-dhawale <dhananjaydhawle2000@gmail.com>
2025-08-02 20:25:41 +0530

    more subtle changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-black/70 backdrop-blur text-white px-6 py-4 flex justify-center items-center">
+    <header className="sticky top-0 z-50 bg-black text-white px-6 py-6 flex justify-center items-center">
       <nav className="space-x-6 text-sm">
         <a href="/astro-portfolio/" className="hover:text-sky-400">Home</a>
         <a href="/astro-portfolio/about" className="hover:text-sky-400">About</a>
