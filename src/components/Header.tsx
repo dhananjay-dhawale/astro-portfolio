175e898bae250f1c3c3d2f994c02f3b51fd530a4
dhananjay-dhawale <dhananjaydhawle2000@gmail.com>
2025-08-03 00:21:28 +0530

    updated coloring of headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-black text-white px-6 py-6 flex justify-center items-center">
       <nav className="space-x-6 text-sm">
-        <a href="/astro-portfolio/" className="hover:text-sky-400">Home</a>
-        <a href="/astro-portfolio/about" className="hover:text-sky-400">About</a>
-        <a href="/astro-portfolio/projects" className="hover:text-sky-400">Projects</a>
-        <a href="/astro-portfolio/connect" className="hover:text-sky-400">Connect</a>
+        <a href="/astro-portfolio/" className="hover:text-purple-300">Home</a>
+        <a href="/astro-portfolio/about" className="hover:text-purple-300">About</a>
+        <a href="/astro-portfolio/projects" className="hover:text-purple-300">Projects</a>
+        <a href="/astro-portfolio/connect" className="hover:text-purple-300">Connect</a>
       </nav>
     </header>
   );
